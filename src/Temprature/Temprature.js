8794832c41a055f1500b3318e51ee7fc582747f8
hp <niranjan962796@gmail.com>
2024-08-21 18:09:15 +0530

    first commit

---
+++ src/Temprature/Temprature.js
@@ -0,0 +1,23 @@
+import "./Temprature.css";
+import { FaLocationDot } from "react-icons/fa6";
+
+
+
+const Temprature=()=>{
+    return(
+        < div className="temp">
+        <div className="temp-1">
+<div>
+    <input typt="text" placeholder="serch" className="temp-inp"></input>
+</div>
+<div className="temp-2">
+<h1 className="location"><FaLocationDot />London</h1>
+</div>
+<div className="temp-3">
+<h2>52.53'Cel</h2>
+</div>
+        </div>
+        </div>
+    )
+}
+export default Temprature;
